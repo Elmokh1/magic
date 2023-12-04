d4229c1ee9ac9345da2b3f7736008f3c43609bdd
Elmokh1 <132287016+Elmokh1@users.noreply.github.com>
2023-12-04 18:48:19 +0200

    magic_bakery

--- lib/home_screen/sections/sections.dart
+++ lib/home_screen/sections/sections.dart
@@ -13,11 +13,11 @@ class Sections extends StatelessWidget {
                 "wish you good health",
                 style: GoogleFonts.mogra(
                   fontSize: 20,
-                  color: Color(0xff65451F),
+                  color: const Color(0xff65451F),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.0),
                 child: Divider(
                   thickness: 3,
                   color: Color(0xffF4DFBA),
@@ -31,194 +31,222 @@ class Sections extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
-                Row(
+                const Row(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("الكل"),
                     ),
                     Spacer(),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("مخبوزات خاليه من السكر"),
                     ),
                   ],
                 ),
-                Container(
+                SizedBox(
                   height: 140,
                   width: double.infinity,
                   child: Directionality(
                     textDirection: TextDirection.rtl,
                     child: ListView.separated(
-                      separatorBuilder: (context, index) => SizedBox(width: 10,),
+                      separatorBuilder: (context, index) => const SizedBox(
+                        width: 10,
+                      ),
                       scrollDirection: Axis.horizontal,
                       itemCount: 20,
                       itemBuilder: (context, index) => Container(
                         width: 240,
                         height: 140,
                         decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xffE5E5E5))
-                        ),
+                            border: Border.all(color: const Color(0xffE5E5E5))),
                         child: Row(
                           children: [
-                            Column(
+                            const Column(
                               children: [
-                                Icon(Icons.favorite_outlined,color: Colors.red,),
+                                Icon(
+                                  Icons.favorite_outlined,
+                                  color: Colors.red,
+                                ),
                               ],
                             ),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Center(
-                                  child: Text("دونتس شيكولاته",style: GoogleFonts.inter(
-                                    color: Color(0xff65451F),
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w400,
-                                  ),),
+                                  child: Text(
+                                    "دونتس شيكولاته",
+                                    style: GoogleFonts.inter(
+                                      color: const Color(0xff65451F),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
                                 ),
-                                Text("20 LE",style: GoogleFonts.inter(
-                                  color: Color(0xff65451F),
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                ),),
+                                Text(
+                                  "20 LE",
+                                  style: GoogleFonts.inter(
+                                    color: const Color(0xff65451F),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
                               ],
                             ),
                             Image.asset("assets/images/Rectangle 29.png"),
-
                           ],
                         ),
                       ),
-
                     ),
                   ),
                 ),
-                SizedBox(height: 20,),
-                Row(
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("الكل"),
                     ),
                     Spacer(),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("مخبوزات خاليه من السكر"),
                     ),
                   ],
                 ),
-                Container(
+                SizedBox(
                   width: double.infinity,
                   height: 140,
-
                   child: Directionality(
                     textDirection: TextDirection.rtl,
                     child: ListView.separated(
-                      separatorBuilder: (context, index) => SizedBox(width: 10,),
+                      separatorBuilder: (context, index) => const SizedBox(
+                        width: 10,
+                      ),
                       scrollDirection: Axis.horizontal,
                       itemCount: 20,
                       itemBuilder: (context, index) => Container(
                         width: 240,
                         height: 140,
                         decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xffE5E5E5))
-                        ),
+                            border: Border.all(color: const Color(0xffE5E5E5))),
                         child: Row(
                           children: [
-                            Column(
+                            const Column(
                               children: [
-                                Icon(Icons.favorite_outlined,color: Colors.red,),
+                                Icon(
+                                  Icons.favorite_outlined,
+                                  color: Colors.red,
+                                ),
                               ],
                             ),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Center(
-                                  child: Text("دونتس شيكولاته",style: GoogleFonts.inter(
-                                    color: Color(0xff65451F),
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w400,
-                                  ),),
+                                  child: Text(
+                                    "دونتس شيكولاته",
+                                    style: GoogleFonts.inter(
+                                      color: const Color(0xff65451F),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
                                 ),
-                                Text("20 LE",style: GoogleFonts.inter(
-                                  color: Color(0xff65451F),
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                ),),
+                                Text(
+                                  "20 LE",
+                                  style: GoogleFonts.inter(
+                                    color: const Color(0xff65451F),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
                               ],
                             ),
                             Image.asset("assets/images/Rectangle 29.png"),
-
                           ],
                         ),
                       ),
-
                     ),
                   ),
                 ),
-                SizedBox(height: 20,),
-                Row(
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("الكل"),
                     ),
                     Spacer(),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(8.0),
                       child: Text("مخبوزات خاليه من السكر"),
                     ),
                   ],
                 ),
-                Container(
+                SizedBox(
                   height: 140,
                   width: double.infinity,
-                  child:Directionality(
+                  child: Directionality(
                     textDirection: TextDirection.rtl,
                     child: ListView.separated(
-                      separatorBuilder: (context, index) => SizedBox(width: 10,),
+                      separatorBuilder: (context, index) => const SizedBox(
+                        width: 10,
+                      ),
                       scrollDirection: Axis.horizontal,
                       itemCount: 20,
                       itemBuilder: (context, index) => Container(
                         width: 240,
                         height: 140,
                         decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xffE5E5E5))
-                        ),
+                            border: Border.all(color: const Color(0xffE5E5E5))),
                         child: Row(
                           children: [
-                            Column(
+                            const Column(
                               children: [
-                                Icon(Icons.favorite_outlined,color: Colors.red,),
+                                Icon(
+                                  Icons.favorite_outlined,
+                                  color: Colors.red,
+                                ),
                               ],
                             ),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Center(
-                                  child: Text("دونتس شيكولاته",style: GoogleFonts.inter(
-                                    color: Color(0xff65451F),
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w400,
-                                  ),),
+                                  child: Text(
+                                    "دونتس شيكولاته",
+                                    style: GoogleFonts.inter(
+                                      color: const Color(0xff65451F),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
                                 ),
-                                Text("20 LE",style: GoogleFonts.inter(
-                                  color: Color(0xff65451F),
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                ),),
+                                Text(
+                                  "20 LE",
+                                  style: GoogleFonts.inter(
+                                    color: const Color(0xff65451F),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
                               ],
                             ),
                             Image.asset("assets/images/Rectangle 29.png"),
-
                           ],
                         ),
                       ),
-
                     ),
                   ),
                 ),
-                SizedBox(height: 20,),
-
+                const SizedBox(
+                  height: 20,
+                ),
               ],
             ),
           ),
